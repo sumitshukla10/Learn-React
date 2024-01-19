6d269b4eb3400b4897a3b3183a048ca8fa18a82b
sumit shukla <9910sumitshukla@gmail.com>
2024-01-19 13:08:03 +0530

    leaning react

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"i am h1"),
+        React.createElement("h2",{},"i am h2"),
+    ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
